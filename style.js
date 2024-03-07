ded11875bbe37a53eaaa59ed819f00ef9623dfc7
Abdulaziz <154592349+abdulazizmughal@users.noreply.github.com>
2024-03-07 14:43:17 +0500

    Add files via upload

---
+++ style.js
@@ -0,0 +1,47 @@
+                                //    Chapter no 2
+// Qustion no 1
+
+var username = "aziz"
+
+// Question no 2
+
+var myname = "MY Full Name";
+
+// Question no 3
+
+var message; 
+message = "Hello World"; 
+alert(message); 
+
+// Question no 4
+
+var studentName = "abdulaziz";
+var age = 18;
+var grade = "A";
+
+alert("Student Name: " + studentName);
+alert("Age: " + age);
+alert("Grade: " + grade);
+
+// Question no 5
+
+alert("pizza\n"+"pizz\n"+"piz\n"+"pi\n"+"p\n");
+
+// Qustion no 6
+var email = "abdulazizmughal776@gmail.com"; 
+alert("My email address is: " + email);
+
+// Question no 7;
+var book = "A smarter way to learn JavaScript";
+alert("I am currently reading the book: '" + book + "'");
+
+// Question no 8
+document.write("<h1>this is my first website<h1/>"
+)
+// Questio no 9
+alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+
+// ====END=====
+
+
+
